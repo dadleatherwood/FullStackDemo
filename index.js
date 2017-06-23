f378c521a08802444b0f38038c72cb9573b6024d
Jacob Leatherwood <jacob.leatherwood.22@gmail.com>
2017-06-23 10:52:44 -0500

    extablished working app

--- index.js
+++ index.js
@@ -7,4 +7,15 @@ const express = require('express')
   , session = require('express-session')
   , config = require('./server/config')
 
-  
+  app.use(cors())
+  app.use(json())
+  app.use(session(config.session))
+
+  app.use('/', express.static(__dirname + '/public'))
+  massive(config.postgres).then(dbInstance => {
+    app.set('db', dbInstance)
+  })
+
+  app.listen(port, function(){
+    console.log("Listening at port 3000")
+  })
