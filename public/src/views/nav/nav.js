d3700c05fdd2c054fb72ec0bceea8e1943264941
Jacob Leatherwood <jacob.leatherwood.22@gmail.com>
2017-06-23 11:56:27 -0500

    created nav and login page

---
+++ public/src/views/nav/nav.js
@@ -0,0 +1,8 @@
+angular.module('app').directive('navDir', function(loginService){
+  return {
+    templateUrl: './src/views/nav/nav.html',
+    controller: function($scope, $state){
+      
+    }
+  }
+})
